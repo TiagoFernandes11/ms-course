495ca5bfb42690400deca85dd5705b2210b477ea
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-18 15:18:26 -0300

    Implementando projeto hr-worker

---
+++ hr-worker/hr-worker/src/main/java/udemy/hrworker/entities/Worker.java
@@ -0,0 +1,22 @@
+package udemy.hrworker.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_worker")
+@Data
+@NoArgsConstructor
+public class Worker implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private double dailyIncome;
+}
---
+++ hr-worker/hr-worker/src/main/java/udemy/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package udemy.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import udemy.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
