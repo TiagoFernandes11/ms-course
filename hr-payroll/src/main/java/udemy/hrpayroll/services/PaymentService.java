5b67d00360f9168f07ed79d186ae146fdd4b5c45
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-20 10:13:15 -0300

    Feign client

--- hr-payroll/src/main/java/udemy/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/udemy/hrpayroll/services/PaymentService.java
@@ -1,22 +1,19 @@
 package udemy.hrpayroll.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import udemy.hrpayroll.entities.Payment;
 import udemy.hrpayroll.entities.Worker;
+import udemy.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
-    public Payment getPayment(long workerId, int days){
-        Worker worker = restTemplate.getForObject(this.workerHost + "/workers/" + workerId, Worker.class);
+    public Payment getPayment(long workerId, int days) {
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         assert worker != null;
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
